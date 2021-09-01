4bba154d2620f0cccf343f9a2879e081fda4ca25
Florian Baaske <Florian.Baaske@hpe.com>
2021-09-01 20:47:04 +0200

    adding front cards

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,23 @@
 function load() {
-    alert('test')
+    addCardsToHtml(9)
+    
+}
+
+function addCardsToHtml(number) {
+    mySection = document.getElementById("memory-game");
+    
+    for (let i = 0; i < number; i++) {
+        myDiv = document.createElement("div");
+        myDiv.setAttribute("class", "memory-card");
+
+        myImg = document.createElement("img");
+        myImg.setAttribute("class", "front-face");
+        myImg.setAttribute("src", "images/front-face.jpeg");
+        myImg.setAttribute("alt", "React");
+
+        myDiv.appendChild(myImg);
+
+        mySection.appendChild(myDiv);
+    }
+
 }
